d25372d69abf648dbceb67052fbcbcc4ca0e9e12
wen911119 <wen911119@gmail.com>
2018-06-15 12:55:40 +0800

    update preact-modal，在fadeout阶段用transition代替keyframes

--- packages/preact-modal/index.js
+++ packages/preact-modal/index.js
@@ -12,22 +12,18 @@ const baseStyle = {
   backgroundColor: 'rgba(0,0,0,0.2)',
   justifyContent: 'center',
   alignItems: 'center',
-  animationDuration: '0.3s'
+  animationDuration: '.3s',
+  transition: 'opacity .3s',
+  opacity: 1
 }
-
 let styleSheet = document.styleSheets[0]
 const keyframesShow = `
     @keyframes mask-fadein {
         0%{opacity: 0} 
         100%{opacity: 1}
     }`
-const keyframesHide = `
-    @keyframes mask-fadeout {
-      0%{opacity: 1} 
-      100%{opacity: 0}
-    }`
 styleSheet.insertRule(keyframesShow, styleSheet.cssRules.length)
-styleSheet.insertRule(keyframesHide, styleSheet.cssRules.length)
+
 function hack (maskClickHander) {
   return function (e) {
     e.target.className.indexOf('_modal_mask_') > -1 &&
@@ -50,27 +46,32 @@ export default class Modal extends Component {
   componentWillReceiveProps (nextProps) {
     if (this.props.open !== nextProps.open) {
       if (!(this.state.close = nextProps.open)) {
-        // 延迟0.29s关闭，在动画结束之前就得关闭，要不然会闪一下。因为提前10毫秒关这时候透明度几乎为0，闪一下看不出来。
-        setTimeout(() => this.setState({ close: true }), 290)
+        setTimeout(() => this.setState({ close: true }), 300)
       }
     }
   }
   render = (
     { open, into = 'body', children, onMaskClick, style = {} },
     { close }
-  ) =>
-    open || !close ? (
+  ) => {
+    let mergedStyle = Object.assign({}, baseStyle, style)
+    if (open) {
+      mergedStyle.animationName = 'mask-fadein'
+    }
+    else {
+      mergedStyle.opacity = 0
+    }
+    return open || !close ? (
       <Portal into={into}>
         <div
           className="_modal_mask_"
           onClick={hack(onMaskClick)}
           onTouchMove={noMove}
-          style={Object.assign({}, baseStyle, style, {
-            animationName: open ? 'mask-fadein' : 'mask-fadeout'
-          })}
+          style={mergedStyle}
         >
           {children}
         </div>
       </Portal>
     ) : null
+  }
 }
