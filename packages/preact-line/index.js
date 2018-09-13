5672689f1e5786b7823148ca5068f20afd429806
wen911119 <wen911119@gmail.com>
2018-09-13 14:58:43 +0800

    update:preact-line

--- packages/preact-line/index.js
+++ packages/preact-line/index.js
@@ -1,5 +1,5 @@
 import { h } from 'preact'
-
+import px2rem from 'p-to-r'
 const scaleDefault = 1 / (window.devicePixelRatio || 1)
 
 const Line = ({
@@ -8,9 +8,16 @@ const Line = ({
   width = '100%',
   height = '100%',
   size = '1px',
-  scale
+  scale,
+  indent = [0, 0]
 }) => {
-  let baseStyle = { backgroundColor: color, width, height }
+  let baseStyle = {
+    backgroundColor: color,
+    width,
+    height,
+    marginLeft: px2rem(indent[0]),
+    marginRight: px2rem(indent[1])
+  }
   if (!v) {
     baseStyle.height = size
     baseStyle.transform = `scaleY(${scale || scaleDefault})`
