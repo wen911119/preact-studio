d56cf0b16f057ea5929b2b30e6866b1e8f1af69c
wen911119 <wen911119@gmail.com>
2018-09-13 17:25:42 +0800

    update:preact-line

--- packages/preact-line/index.js
+++ packages/preact-line/index.js
@@ -5,28 +5,39 @@ const scaleDefault = 1 / (window.devicePixelRatio || 1)
 const Line = ({
   color = '#eaeaea',
   v = false,
-  width = '100%',
-  height = '100%',
+  // width = '100%',
+  // height = '100%',
   size = '1px',
   scale,
   indent = [0, 0]
 }) => {
+  let wrapStyle = {
+    boxSizing: 'border-box'
+  }
   let baseStyle = {
     backgroundColor: color,
-    width,
-    height,
-    marginLeft: px2rem(indent[0]),
-    marginRight: px2rem(indent[1])
+    width: '100%',
+    height: '100%'
   }
   if (!v) {
     baseStyle.height = size
     baseStyle.transform = `scaleY(${scale || scaleDefault})`
+    wrapStyle.paddingLeft = px2rem(indent[0])
+    wrapStyle.paddingRight = px2rem(indent[1])
+    wrapStyle.width = '100%'
   }
   else if (v) {
     baseStyle.width = size
     baseStyle.transform = `scaleX(${scale || scaleDefault})`
+    wrapStyle.paddingTop = px2rem(indent[0])
+    wrapStyle.paddingBottom = px2rem(indent[1])
+    wrapStyle.height = '100%'
   }
-  return <div style={baseStyle} />
+  return (
+    <div style={wrapStyle}>
+      <div style={baseStyle} />
+    </div>
+  )
 }
 
 export default Line
