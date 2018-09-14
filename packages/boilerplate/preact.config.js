c9c4ce18be92bc8dcab3506638ee0cb4c21988d0
wen911119 <wen911119@gmail.com>
2018-09-14 10:50:55 +0800

    修改build配置

--- packages/boilerplate/preact.config.js
+++ packages/boilerplate/preact.config.js
@@ -12,6 +12,14 @@ export default function (config, env, helpers) {
   // config.entry.home = path.resolve('./src/pages/home/app.js')
   // config.entry.bundle[0] = path.resolve('./src/pages/home/app.js')
   // console.log(config.module.loaders[0].options.plugins, 6666666)
-  return config
+  if (env.isProd) {   
+
+    // Make async work
+    let babel = config.module.loaders.filter( loader => loader.loader === 'babel-loader')[0].options;
+    // Blacklist regenerator within env preset:
+    babel.presets[0][1].exclude.push('transform-async-to-generator');
+    // Add fast-async
+    babel.plugins.push([require.resolve('fast-async'), { spec: true }]);
+  }
   // console.log(path.resolve('./src/pages/home/app.js'), 6666666)
 }
