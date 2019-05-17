5d0b7df8b98ee95e897be490741f263440ad3e8f
wen911119 <wen911119@gmail.com>
2019-05-17 10:52:49 +0800

    feat:layout-suite重构

--- packages/preact-layout-suite/index.js
+++ packages/preact-layout-suite/index.js
@@ -1,7 +1,61 @@
 import { h } from 'preact'
-import _px2rem from 'p-to-r'
+import px2rem from 'p-to-r'
+import layout from './layout.css'
 
-const px2rem = px => typeof px === 'number' ? _px2rem(px) : px
+const styleEl = document.createElement('style')
+let injectedStyle = ''
+for (let i = 1; i <= 500; i++) {
+  injectedStyle += `.child-mgr-${i}>*{margin-right:${px2rem(
+    i
+  )}}.child-mgb-${i}>*{margin-bottom:${px2rem(i)}}`
+}
+styleEl.innerHTML = injectedStyle
+document.head.appendChild(styleEl)
+const styleParser = ({ padding, margin, bgColor, width, height }) => {
+  let composeStyle = {}
+  if (padding) {
+    composeStyle = Object.assign(composeStyle, {
+      paddingTop: px2rem(padding[0]),
+      paddingRight: px2rem(padding[1]),
+      paddingBottom: px2rem(padding[2]),
+      paddingLeft: px2rem(padding[3])
+    })
+  }
+  if (margin) {
+    composeStyle = Object.assign(composeStyle, {
+      marginTop: px2rem(margin[0]),
+      marginRight: px2rem(margin[1]),
+      marginBottom: px2rem(margin[2]),
+      marginLeft: px2rem(margin[3])
+    })
+  }
+  if (bgColor) {
+    composeStyle.backgroundColor = bgColor
+  }
+  if (width) {
+    composeStyle.width = px2rem(width)
+  }
+  if (height) {
+    composeStyle.height = px2rem(height)
+  }
+  return composeStyle
+}
+
+const classNameParser = type => ({ vAlign, hAlign, className }) => {
+  let classNames = [layout[type]]
+  if (vAlign) {
+    const classObject = layout[`${type}-v-${vAlign}`]
+    classObject && classNames.push(classObject)
+  }
+  if (hAlign) {
+    const classObject = layout[`${type}-h-${hAlign}`]
+    classObject && classNames.push(classObject)
+  }
+  if (className) {
+    classNames.push(className)
+  }
+  return classNames.join(' ')
+}
 
 function alternateInsert (arr, item) {
   let insertedArr = arr.reduce((all, current) => all.concat(current, item), [])
@@ -11,138 +65,130 @@ function alternateInsert (arr, item) {
 
 export const RowView = ({
   children,
-  vAlign = 'center',
-  hAlign = 'flex-start',
-  height = 'initial',
-  width = 'initial',
-  bgColor = 'transparent',
-  padding = [0, 0, 0, 0],
-  margin = [0, 0, 0, 0],
+  vAlign,
+  hAlign,
+  height,
+  width,
+  bgColor,
+  padding,
+  margin,
   style = {},
-  className = ''
-}) => (
-  <div
-    className={className}
-    style={Object.assign(
-      {
-        boxSizing: 'border-box',
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: vAlign,
-        justifyContent: hAlign,
-        backgroundColor: bgColor,
-        paddingTop: px2rem(padding[0]),
-        paddingRight: px2rem(padding[1]),
-        paddingBottom: px2rem(padding[2]),
-        paddingLeft: px2rem(padding[3]),
-        marginTop: px2rem(margin[0]),
-        marginRight: px2rem(margin[1]),
-        marginBottom: px2rem(margin[2]),
-        marginLeft: px2rem(margin[3]),
-        height: px2rem(height),
-        width: px2rem(width)
-      },
-      style
-    )}
-  >
-    {children}
-  </div>
-)
+  className
+}) => {
+  const classNames = classNameParser('rowview')({ vAlign, hAlign, className })
+  const composeStyle = styleParser({ padding, margin, bgColor, width, height })
+  return (
+    <div className={classNames} style={Object.assign(composeStyle, style)}>
+      {children}
+    </div>
+  )
+}
 
 export const ColumnView = ({
   children,
-  vAlign = 'initial',
-  hAlign = 'initial',
-  padding = [0, 0, 0, 0],
-  margin = [0, 0, 0, 0],
-  bgColor = 'transparent',
-  width = 'initial',
-  height = 'initial',
+  vAlign,
+  hAlign,
+  padding,
+  margin,
+  bgColor,
+  width,
+  height,
   style = {},
-  className = ''
+  className
 }) => {
-  let mergedStyle = Object.assign(
-    {
-      boxSizing: 'border-box',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: hAlign,
-      justifyContent: vAlign,
-      backgroundColor: bgColor,
-      paddingTop: px2rem(padding[0]),
-      paddingRight: px2rem(padding[1]),
-      paddingBottom: px2rem(padding[2]),
-      paddingLeft: px2rem(padding[3]),
-      marginTop: px2rem(margin[0]),
-      marginRight: px2rem(margin[1]),
-      marginBottom: px2rem(margin[2]),
-      marginLeft: px2rem(margin[3]),
-      height: px2rem(height),
-      width: px2rem(width)
-    },
-    style
-  )
+  const classNames = classNameParser('columnview')({
+    vAlign,
+    hAlign,
+    className
+  })
+  const composeStyle = styleParser({ padding, margin, bgColor, width, height })
   return (
-    <div className={className} style={mergedStyle}>
+    <div className={classNames} style={Object.assign(composeStyle, style)}>
       {children}
     </div>
   )
 }
 
 export const XCenterView = ({
-  className = '',
+  className,
   children,
   style = {},
-  height = 'initial',
-  width = 'initial',
-  bgColor = 'transparent'
-}) => (
-  <div
-    className={className}
-    style={Object.assign(
-      {
-        boxSizing: 'border-box',
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: bgColor,
-        height: px2rem(height),
-        width: px2rem(width)
-      },
-      style
-    )}
-  >
-    {children}
-  </div>
-)
+  height,
+  width,
+  bgColor
+}) => {
+  let classNames = layout.xcenter
+  if (className) {
+    classNames = classNames + ' ' + className
+  }
+  const composeStyle = styleParser({ bgColor, width, height })
+  return (
+    <div className={classNames} style={Object.assign(composeStyle, style)}>
+      {children}
+    </div>
+  )
+}
 
 export const SpaceHolder = ({ width = 0, height = 0 }) => (
   <div style={{ width: px2rem(width), height: px2rem(height) }} />
 )
 
-export const SlotRowView = ({ slot, children, ...otherProps }) => {
+export const SlotRowView = ({
+  slot,
+  legacy,
+  children,
+  className,
+  ...otherProps
+}) => {
   let allChildren = children
   if (slot) {
     if (typeof slot === 'number') {
-      allChildren = alternateInsert(children, <SpaceHolder width={slot} />)
+      if (legacy) {
+        // 保留旧的插div占空间的模式
+        allChildren = alternateInsert(children, <SpaceHolder width={slot} />)
+      }
+      else {
+        className = className ? className + ' ' : ''
+        className = className + `child-mgr-${slot} ${layout.slotrowview}`
+      }
     }
     else {
       allChildren = alternateInsert(children, slot)
     }
   }
-  return <RowView {...otherProps}>{allChildren}</RowView>
+  return (
+    <RowView className={className} {...otherProps}>
+      {allChildren}
+    </RowView>
+  )
 }
 
-export const SlotColumnView = ({ slot = 0, children, ...otherProps }) => {
+export const SlotColumnView = ({
+  slot,
+  legacy,
+  className,
+  children,
+  ...otherProps
+}) => {
   let allChildren = children
   if (slot) {
     if (typeof slot === 'number') {
-      allChildren = alternateInsert(children, <SpaceHolder height={slot} />)
+      if (legacy) {
+        // 保留旧的插div占空间的模式
+        allChildren = alternateInsert(children, <SpaceHolder height={slot} />)
+      }
+      else {
+        className = className ? className + ' ' : ''
+        className = className + `child-mgb-${slot} ${layout.slotcolumnview}`
+      }
     }
     else {
       allChildren = alternateInsert(children, slot)
     }
   }
-  return <ColumnView {...otherProps}>{allChildren}</ColumnView>
+  return (
+    <ColumnView className={className} {...otherProps}>
+      {allChildren}
+    </ColumnView>
+  )
 }
