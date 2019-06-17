7146177b399e476b94ebd74227a1b8549f07e040
wen911119 <wen911119@gmail.com>
2019-06-17 16:54:10 +0800

    删除不需要git管理的文件

--- packages/cli/webpack/entries/tabsDemo.js
+++
@@ -1,9 +0,0 @@
-
-    const { h, render } = require('preact')
-    let App = require('/Users/user/Documents/preact-studio/packages/demo/src/pages/tabsDemo/app.js')
-      .default
-    let root = document.getElementById('app')
-    if (typeof App === 'function') {
-      render(h(App), root || document.body)
-    }
-      
