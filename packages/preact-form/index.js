a2f6dbfa674f05eebae9355dd2ef96b1bac05388
wen911119 <wen911119@gmail.com>
2019-06-20 18:25:59 +0800

    feat:form组件

---
+++ packages/preact-form/index.js
@@ -0,0 +1,204 @@
+import { h, Component, createContext, cloneElement } from 'preact'
+import safeGet from 'dlv'
+
+const FormContext = createContext({
+  subscribeValidate: () => {},
+  update: () => {},
+  onChange: () => {},
+  parentData: {}
+})
+// eslint-disable-next-line
+const withFormContext = Component => props => (
+  <FormContext.Consumer>
+    {context => <Component {...props} {...context} />}
+  </FormContext.Consumer>
+)
+
+@withFormContext
+export class FormField extends Component {
+  state = {
+    err: ''
+  }
+  update = value => {
+    const { field, update } = this.props
+    update(field, value, [field])
+    if (this.state.err) {
+      // 用户修改了，如果之前有错误提示的话，需要清空错误提示
+      this.setState({ err: '' })
+    }
+  }
+  componentDidMount () {
+    const self = this
+    const { validate = [], subscribeValidate } = self.props
+    if (validate.length > 0) {
+      subscribeValidate(
+        () =>
+          new Promise(async (resolve, reject) => {
+            const {
+              validate,
+              label,
+              field,
+              parentData,
+              getFormData
+            } = self.props
+            let err
+            for (let rule of validate) {
+              err = await rule(parentData[field], getFormData)
+              if (err) {
+                break
+              }
+            }
+            if (err) {
+              this.setState({ err })
+              const errMsg = label + err + ''
+              resolve(errMsg)
+            }
+            else {
+              resolve()
+            }
+          })
+      )
+    }
+  }
+  shouldComponentUpdate (nextProps, nextState) {
+    let shouldUpdate =
+      this.props.parentData[this.props.field] !==
+        nextProps.parentData[nextProps.field] ||
+      this.props.label !== nextProps.label ||
+      this.props.field !== nextProps.field ||
+      this.state.err !== nextState.err
+
+    if (!shouldUpdate && nextProps.link) {
+      for (let key in nextProps.link) {
+        if (nextProps.link.hasOwnProperty(key)) {
+          if (nextProps.link[key] === nextProps.lastUpdate) {
+            shouldUpdate = true
+            break
+          }
+        }
+      }
+    }
+    return shouldUpdate
+  }
+  render () {
+    const { children, label, field, parentData, link, getFormData } = this.props
+    let linkData = {}
+    if (link) {
+      const formData = getFormData()
+      for (let key in link) {
+        if (link.hasOwnProperty(key)) {
+          linkData[key] = safeGet(formData, link[key])
+        }
+      }
+    }
+    return cloneElement(children, {
+      value: parentData[field],
+      label,
+      err: this.state.err,
+      sync: this.update,
+      ...linkData
+    })
+  }
+}
+
+@withFormContext
+export class FormFragment extends Component {
+  update = (key, value, keypathsArr) => {
+    const { namespace, parentData, update } = this.props
+    update(
+      namespace,
+      Object.assign({}, parentData[namespace], {
+        [key]: value
+      }),
+      [namespace].concat(keypathsArr)
+    )
+  }
+  render () {
+    const {
+      children,
+      namespace,
+      parentData,
+      subscribeValidate,
+      getFormData,
+      lastUpdate
+    } = this.props
+    return (
+      <FormContext.Provider
+        value={{
+          parentData: parentData[namespace] || {},
+          update: this.update,
+          subscribeValidate,
+          getFormData,
+          lastUpdate
+        }}
+      >
+        {children}
+      </FormContext.Provider>
+    )
+  }
+}
+
+export default class Form extends Component {
+  static Fragment = FormFragment
+  static Field = FormField
+  init = initFormData => {
+    this.setState({
+      formData: initFormData || {},
+      lastUpdate: ''
+    })
+  }
+  validate = (successCallback, failCallback) => {
+    Promise.all(this.validateListeners.map(func => func()))
+      .then(results => {
+        if (results.some(result => result)) {
+          // 有校验失败的
+          failCallback && failCallback(results.filter(result => result))
+        }
+        else {
+          successCallback && successCallback(this.state.formData)
+        }
+      })
+      .catch(error => {
+        // 校验代码执行出错
+        console.log(error)
+      })
+  }
+  getFormData = () => this.state.formData
+  subscribeValidate = callback => {
+    this.validateListeners.push(callback)
+  }
+  update = (key, value, keypathsArr) => {
+    let newFormData = { ...this.state.formData }
+    newFormData[key] = value
+    this.setState({
+      formData: newFormData,
+      lastUpdate: keypathsArr.join('.')
+    })
+  }
+  constructor (props) {
+    super(props)
+    this.changeListeners = []
+    this.validateListeners = []
+    this.state = {
+      formData: {},
+      lastUpdate: ''
+    }
+  }
+
+  render () {
+    const { children } = this.props
+    return (
+      <FormContext.Provider
+        value={{
+          parentData: this.state.formData,
+          lastUpdate: this.state.lastUpdate,
+          update: this.update,
+          subscribeValidate: this.subscribeValidate,
+          getFormData: this.getFormData
+        }}
+      >
+        {children}
+      </FormContext.Provider>
+    )
+  }
+}
