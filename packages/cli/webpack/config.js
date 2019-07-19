fbea0a3a1a562d5375ca8ae471d53199b992523b
wen911119 <wen911119@gmail.com>
2019-07-19 12:01:35 +0800

    fiX#不需要react-hot-loader

--- packages/cli/webpack/config.js
+++ packages/cli/webpack/config.js
@@ -39,8 +39,8 @@ const genEntry = (appJsPath, pageName) => {
     require('preact/debug');
     let App = require('${appJsPath}')
       .default;
-    const hotLoader = require('preact-hot-loader').default;
-    hotLoader.preact(require('preact'));
+    // const hotLoader = require('preact-hot-loader').default;
+    // hotLoader.preact(require('preact'));
     if (typeof App === 'function') {
       let init = () => {
         let _app = require('${appJsPath}')
