613297e1159a9b9c31c602f9c5920ce1ae1b1aa5
wen911119 <wen911119@gmail.com>
2019-08-23 18:19:19 +0800

    update:search-picker

--- packages/preact-m-search-picker/index.js
+++ packages/preact-m-search-picker/index.js
@@ -1,22 +1,55 @@
 import { h, Component, cloneElement } from 'preact'
 import { WithModal } from '@ruiyun/preact-modal'
+import { TextButton } from '@ruiyun/preact-button'
 import Search from '@ruiyun/preact-m-search'
 
-// eslint-disable-next-line
-const renderModalContent = config => () => <Search {...config} />
 const SEARCH_HEIGHT = window.innerHeight + 'px'
+
+// eslint-disable-next-line
+const renderModalContent = ({ searchbar, autolist, slot }) => () => (
+  <div
+    style={{ width: '100vw', height: SEARCH_HEIGHT, backgroundColor: '#fff' }}
+  >
+    <Search searchbar={searchbar} autolist={autolist} slot={slot} />
+  </div>
+)
 
 @WithModal
 export class SearchPicker extends Component {
-  show = config => {
-    const c = renderModalContent(Object.assign({
-      height: SEARCH_HEIGHT
-    }, config))
-    this.props.$modal.show({
-      content: c,
-      position: 'bottom',
-      allowContentTouchMove: true
-    })
+  show = ({ searchbar = {}, autolist = {}, slot } = {}) => {
+    const { fetchListData, renderItem, keyExtractor, format } = autolist
+    if (fetchListData && renderItem && keyExtractor && format) {
+      const mergedSearchBarConfig = Object.assign(
+        {
+          renderRight: () => (
+            <TextButton
+              textSize={24}
+              textColor="#ccc"
+              onClick={this.props.$modal.hide}
+            >
+              取消
+            </TextButton>
+          ),
+          placeholder: '请输入关键字搜索'
+        },
+        searchbar
+      )
+      const content = renderModalContent({
+        searchbar: mergedSearchBarConfig,
+        autolist,
+        slot
+      })
+      this.props.$modal.show({
+        content,
+        position: 'bottom',
+        allowContentTouchMove: true
+      })
+    }
+    else {
+      console.warn(
+        'autolist的配置fetchListData, renderItem, keyExtractor, format缺一不可'
+      )
+    }
   }
   render ({ children }) {
     return cloneElement(children, {
