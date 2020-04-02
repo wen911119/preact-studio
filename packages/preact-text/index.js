800433b3671f151214f02392240a5c71bb764caf
wen911119 <wen911119@gmail.com>
2020-04-02 11:27:45 +0800

    feat:升级text组件

--- packages/preact-text/index.js
+++ packages/preact-text/index.js
@@ -1,13 +1,27 @@
 import { h } from 'preact'
 import px2rem from 'p-to-r'
-const Text = ({ children, size = 30, color = '#333', weight, style = {}, ...otherProps }) => (
+
+// lineHeight 默认改为1，为了更精准的还原设计稿 2020-04-02 11:27
+const Text = ({
+  children,
+  size = 30,
+  color = '#333',
+  weight = 'initial',
+  style = {},
+  lineHeight = 1,
+  ...otherProps
+}) => (
   <span
     {...otherProps}
     style={Object.assign(
-      { color, fontSize: px2rem(size), fontWeight: weight || 'initial' },
+      {
+        color,
+        fontSize: px2rem(size),
+        fontWeight: weight,
+        lineHeight
+      },
       style
     )}
-    
   >
     {children}
   </span>
