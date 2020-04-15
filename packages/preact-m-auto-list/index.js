4c2b17500caaed331e3c78530fb58ceeb65a51a5
wen911119 <wen911119@gmail.com>
2020-04-15 11:47:03 +0800

    feat:列表增加自动回收以提高超长列表的性能

--- packages/preact-m-auto-list/index.js
+++ packages/preact-m-auto-list/index.js
@@ -4,6 +4,30 @@ import {
   ScrollerWithRefreshAndLoadMore
 } from '@ruiyun/preact-m-scroller'
 
+import classNames from './index.css'
+
+const throttle = function(fn, delay, atleast) {
+  let timer = null
+  let previous = null
+
+  return function() {
+    const now = +new Date()
+
+    if (!previous) previous = now
+
+    if (now - previous > atleast) {
+      fn()
+      // 重置上一次开始时间为本次结束时间
+      previous = now
+    } else {
+      clearTimeout(timer)
+      timer = setTimeout(function() {
+        fn()
+      }, delay)
+    }
+  }
+}
+
 const isEqual = (obj1, obj2) => {
   // 这里不用lodash.isequal
   // 换成JSON.stringify对比，减小体积
@@ -26,15 +50,52 @@ class ListItem extends Component {
 }
 
 class ListFragment extends Component {
-  shouldComponentUpdate() {
-    return false
+  state = {
+    hide: false
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.hide !== this.state.hide
+  }
+
+  componentDidMount() {
+    const { scroller, recycleThreshold = 1000 } = this.props
+    this.fragmentHeight = this.f.clientHeight
+    scroller.addEventListener(
+      'scroll',
+      throttle(
+        () => {
+          const myPosition = this.f.getBoundingClientRect()
+          if (
+            myPosition.bottom < -recycleThreshold ||
+            myPosition.top > recycleThreshold
+          ) {
+            this.setState({
+              hide: true
+            })
+          } else {
+            this.setState({
+              hide: false
+            })
+          }
+        },
+        10,
+        100
+      )
+    )
   }
 
   render() {
     const { data, fragmentId, keyExtractor, renderItem } = this.props
+    const { hide } = this.state
 
     return (
-      <div data-list-fragment-id={fragmentId}>
+      <div
+        data-list-fragment-id={fragmentId}
+        ref={f => (this.f = f)}
+        style={{ height: this.fragmentHeight }}
+        className={hide ? classNames.fragmentHide : classNames.fragmentShow}
+      >
         {data.map((d, index) => (
           <ListItem
             data={d}
@@ -60,7 +121,9 @@ class List extends Component {
       renderLoadingView,
       keyExtractor,
       itemClickHandler,
-      renderItem
+      renderItem,
+      scroller,
+      recycleThreshold
     } = this.props
     if (!data) {
       return renderLoadingView()
@@ -77,6 +140,8 @@ class List extends Component {
               key={index}
               keyExtractor={keyExtractor}
               renderItem={renderItem}
+              scroller={scroller}
+              recycleThreshold={recycleThreshold}
             />
           ))}
         </div>
@@ -165,6 +230,7 @@ class Base extends Component {
 
   componentDidMount() {
     this.fecthListData()
+    this.scrollTo = this.s.scrollTo
   }
 
   componentDidUpdate(prevProps) {
@@ -182,7 +248,8 @@ export default class AutoList extends Base {
       renderItem,
       FooterView,
       HeaderView,
-      height
+      height,
+      recycleThreshold
     } = this.props
     const { data, nomore, loading } = this.state
     return (
@@ -192,6 +259,7 @@ export default class AutoList extends Base {
         nomore={nomore}
         loading={loading}
         height={height}
+        ref={s => (this.s = s)}
       >
         {HeaderView && <HeaderView />}
         <List
@@ -201,6 +269,8 @@ export default class AutoList extends Base {
           data={data}
           renderEmptyView={this.renderEmptyView}
           renderLoadingView={this.renderLoadingView}
+          scroller={this.s && this.s.base.children[0]}
+          recycleThreshold={recycleThreshold}
         />
 
         {FooterView && <FooterView />}
@@ -212,7 +282,13 @@ export default class AutoList extends Base {
 // 彻底分为了tree-shaking
 export class AutoListWithRefresh extends Base {
   render() {
-    const { keyExtractor, renderItem, FooterView, HeaderView } = this.props
+    const {
+      keyExtractor,
+      renderItem,
+      FooterView,
+      HeaderView,
+      recycleThreshold
+    } = this.props
     const { data, nomore, loading } = this.state
     return (
       <ScrollerWithRefreshAndLoadMore
@@ -220,6 +296,7 @@ export class AutoListWithRefresh extends Base {
         onRefresh={this.onRefresh}
         nomore={nomore}
         loading={loading}
+        ref={s => (this.s = s)}
       >
         {HeaderView && <HeaderView />}
         <List
@@ -229,6 +306,8 @@ export class AutoListWithRefresh extends Base {
           data={data}
           renderEmptyView={this.renderEmptyView}
           renderLoadingView={this.renderLoadingView}
+          scroller={this.s && this.s.base.children[0]}
+          recycleThreshold={recycleThreshold}
         />
 
         {FooterView && <FooterView />}
