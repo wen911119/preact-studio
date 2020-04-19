53fc06dde8fbdc7d48b1063d3ed0bc4a8462f177
wen911119 <wen911119@gmail.com>
2020-04-19 22:08:57 +0800

    fix#text组件行高改回默认

--- packages/preact-text/index.js
+++ packages/preact-text/index.js
@@ -1,14 +1,13 @@
 import { h } from 'preact'
 import px2rem from 'p-to-r'
 
-// lineHeight 默认改为1，为了更精准的还原设计稿 2020-04-02 11:27
 const Text = ({
   children,
   size = 30,
   color = '#333',
   weight = 'initial',
-  style = {},
-  lineHeight = 1,
+  style,
+  lineHeight = 'normal',
   ...otherProps
 }) => (
   <span
